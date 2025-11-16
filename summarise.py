29cc0fae2c1e718d7c6095d19338471822d1e868
Daksh Choudhary <132166506+DakshC17@users.noreply.github.com>
2025-11-16 21:15:45 +0530

    Update summarise.py

--- summarise.py
+++ summarise.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 if not OPENROUTER_API_KEY:
     raise ValueError("❌ OPENROUTER_API_KEY is missing. Make sure it's defined in your .env file.")
